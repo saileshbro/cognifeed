911e7e4c6b99d3ffbae9756a6b056c2f767d5bf5
Sailesh Dahal <saileshbro@gmail.com>
2019-12-01 00:03:51 +0545

    Authentication login and signup addeed

---
+++ src/database/database.js
@@ -0,0 +1,30 @@
+const mysql = require('mysql2');
+const util = require('util');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.error('Database connection was closed.');
+    }
+    if (err.code === 'ER_CON_COUNT_ERROR') {
+      console.error('Database has too many connections.');
+    }
+    if (err.code === 'ECONNREFUSED') {
+      console.error('Database connection was refused.');
+    }
+  }
+
+  if (connection) connection.release();
+});
+pool.query = util.promisify(pool.query);
+module.exports = pool;
