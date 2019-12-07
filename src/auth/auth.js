b3c18ba708833a0f55c9149e0a29fdb430193a36
Sailesh Dahal <saileshbro@gmail.com>
2019-12-07 14:44:18 +0545

    Added auth middleware

---
+++ src/auth/auth.js
@@ -0,0 +1,26 @@
+/* eslint-disable require-atomic-updates */
+const jwt = require("jsonwebtoken")
+const pool = require("../database/database")
+const { ErrorHandler } = require("../helpers/error_handler")
+module.exports = async (req, res, next) => {
+  try {
+    if (!req.header("Authorization") || req.header("Authorization") === "Bearer null") {
+      throw new ErrorHandler(401, "Please authenticate.")
+    }
+    const token = req.header("Authorization").replace("Bearer ", "")
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    let user = await pool.query("SELECT * FROM users WHERE user_id=?", [decoded.user_id])
+    if (user.length == 0) {
+      throw new ErrorHandler(401, "Please authenticate.")
+    }
+    req.token = token
+    user = user[0]
+    delete user.password
+    delete user.created_at
+    delete user.updated_at
+    req.user = user
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
--- src/helpers/error_handler.js
+++ src/helpers/error_handler.js
@@ -1,14 +1,23 @@
 class ErrorHandler extends Error {
+  /**
+   *
+   * @param {number} statusCode
+   * @param {String} error
+   */
   constructor(statusCode, error) {
     super()
-
     this.statusCode = statusCode
     this.error = error
   }
 }
+/**
+ *
+ * @param {ErrorHandler} err
+ * @param {*} res
+ */
 const errorHandler = (err, res) => {
   const { statusCode, error } = err
-  res.status(statusCode || 500).json({ error })
+  res.status(statusCode || 500).json({ error: error || "Error Connecting to Database." })
 }
 
 module.exports = { ErrorHandler, errorHandler }
