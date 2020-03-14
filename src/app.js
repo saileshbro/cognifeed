8f928b27b791aeb34328641307e514659278fe24
Sarayu Gautam <sarayugautam1@gmail.com>
2020-03-14 13:41:27 +0545

    firebase

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 require("dotenv").config()
+const axios = require("axios")
 const express = require("express")
 const app = express()
 const morgan = require("morgan")
 const bodyParser = require("body-parser")
 const { errorHandler } = require("./helpers/error_handler")
+const fcm = require("../functions/index")
 const port = process.env.PORT || 8000
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -26,6 +28,7 @@ app.listen(port, console.log(`Server running on port ${port}`))
 app.use((err, req, res, next) => {
   errorHandler(err, res)
 })
+
 //Spider Instantiation.
 // const Link = require("./scraper/link")
 // const Spider = require("./scraper/spider")
@@ -38,3 +41,20 @@ app.use((err, req, res, next) => {
 //   purifier.purify()
 //   purifier.persistPurified()
 // })()
+const sendNotification = async () => {
+  try {
+    const response = await axios({
+      method: "POST",
+      url: "https://fcm.googleapis.com/fcm/send",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Key=AAAAZ6-SYm0:APA91bGiJAyN79c-AqVRf94wiKia1ghqmNk6-xTW1_OadiF-OaTxAo9XclHrm7d5bN74yBzQAPUj38BX32ygx0Uq35Pq8_3VsK5c7AMHiSh6joPKlT98Ju82iimVn41P_BPK138Bf0k5"
+      },
+      data: { notification: { title: "My Awesome App", body: "message" } }
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+sendNotification()
